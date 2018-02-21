149d2992a50d41f83fe1f9ff3088442f371ca28a
Dracologist <ejckanzler@yahoo.com>
2018-02-20 23:59:34 -0500

    Altering test to show both

--- index.php
+++ index.php
@@ -8,6 +8,15 @@ include("validation.php");
 $parts = array("AP-12-3507", "  ap-99-X109  ", "SG-05-ab20",
     "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
 foreach ($parts as $part) {
+    if (validPart($part)) {
+        echo "$part is valid.<br>";
+    }
+    else{
+        echo "$part is not valid.<br>";
+    }
+}
+echo "<br>";
+foreach ($parts as $part) {
     if (validPartRx($part)) {
         echo "$part is valid.<br>";
     }
