e0dad0bf5acaf56c2d33d72b939d61d6e5e185ec
Dracologist <ejckanzler@yahoo.com>
2018-02-20 22:35:06 -0500

    Validation without regex completed

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ejcka
+ * Date: 2/20/2018
+ */
+include("validation.php");
+$parts = array("AP-12-3507", "  ap-99-X109  ", "SG-05-ab20",
+    "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
+foreach ($parts as $part) {
+    if (validPart($part)) {
+        echo "$part is valid.<br>";
+    }
+    else{
+        echo "$part is not valid.<br>";
+    }
+}
--- validation.php
+++ validation.php
@@ -5,6 +5,23 @@
  * Date: 2/20/2018
  */
 
-function validPart($part){
-
+function validPart($input){
+    $input = trim($input);
+    $pieces = explode("-", $input);
+    $category = $pieces[0];
+    $warehouse = $pieces[1];
+    $part = $pieces[2];
+    $category = strtoupper($category);
+    if($category != "HW" && $category != "SG" && $category != "AP"){
+        return false;
+    }
+    elseif (!is_numeric($warehouse) || strlen($warehouse) != 2){
+        return false;
+    }
+    elseif (!ctype_alnum($part) || strlen($part) != 4){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
